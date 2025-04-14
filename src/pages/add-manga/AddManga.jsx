e4f332260b7098557da4c5ae2e06f51b9c794081
Lucas Lima <mosukasangamer@gmail.com>
2025-04-14 14:30:23 -0300

    No autocomplete on addManga

--- src/pages/add-manga/AddManga.jsx
+++ src/pages/add-manga/AddManga.jsx
@@ -41,23 +41,23 @@ function AddManga() {
                         <form id="create-manga-form" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="titulo">Título</label><br />
-                                <input type="text" id="titulo" name="titulo" value={formData.titulo} onChange={handleChange}/><br />
+                                <input type="text" id="titulo" name="titulo" value={formData.titulo} onChange={handleChange} autoComplete="off"/><br />
                             </div>
                             <div>
                                 <label htmlFor="imagem">Imagem</label><br />
-                                <input type="text" id="imagem" name="imagem" value={formData.imagem} onChange={handleChange}/><br />
+                                <input type="text" id="imagem" name="imagem" value={formData.imagem} onChange={handleChange} autoComplete="off"/><br />
                             </div>
                             <div>
                                 <label htmlFor="sinopse">Sinopse</label><br />
-                                <input type="text" id="sinopse" name="sinopse" value={formData.sinopse} onChange={handleChange} />
+                                <input type="text" id="sinopse" name="sinopse" value={formData.sinopse} onChange={handleChange} autoComplete="off"/>
                             </div>
                             <div>   
                                 <label htmlFor="autor">Autor</label><br />
-                                <input type="text" id="autor" name="autor" value={formData.autor} onChange={handleChange}/><br />
+                                <input type="text" id="autor" name="autor" value={formData.autor} onChange={handleChange} autoComplete="off"/><br />
                             </div>
                             <div>
                                 <label htmlFor="tipo">Tipo</label><br />
-                                <input type="text" id="tipo" name="tipo" value={formData.tipo} onChange={handleChange}/><br />
+                                <input type="text" id="tipo" name="tipo" value={formData.tipo} onChange={handleChange} autoComplete="off"/><br />
                             </div>
                             <div className="center-element">
                                 <button type="submit">Enviar</button>
